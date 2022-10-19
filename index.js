7a05d5bd31e46fbf8014756d7c23280845153218
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-10-18 20:25:10 -0500

    adjusted some css

--- index.js
+++ index.js
@@ -27,31 +27,39 @@ const pokemonType = document.getElementById("type");
 //         );
 // });
 
-for (let i = 1; i <= 152; i++) {
+for (let i = 1; i <= 151; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
         .then((response) => response.json())
         .then((data) => {
             for (let i = 1; i <= 1; i++) {
                 document.getElementById("container").innerHTML += `
-                    <div class="card" style="width: 18rem">
-                    <img id="image" class="card-img-top" src="${data.sprites.front_default}" />
-                    <div class="card-body">
-                        <h5 class="card-title" id="name">Name: ${data.species.name}</h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">HP: ${data.stats[0].base_stat}</li>
-                        <li class="list-group-item">Attack: ${data.stats[1].base_stat}</li>
-                        <li class="list-group-item">Defense: ${data.stats[2].base_stat} </li>
-                        <li class="list-group-item">Type: ${data.types[0].type.name}</li>
-                    </ul>
-                </div>`;
+                <div class="card shadow-lg p-3 mb-5 bg-body rounded" style="width: 15rem">
+                <img
+                    id="image"
+                    class="card-img-top"
+                    src="${data.sprites.front_default}"
+                />
+                <div class="card-body">
+                    <h3 class="card-title" id="name">
+                        ${data.species.name}
+                        </h3>
+                        HP - ${data.stats[0].base_stat}
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" aria-label="Success example" style="width: ${data.stats[0].base_stat}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                          Attack - ${data.stats[1].base_stat}
+                        <div class="progress">
+                            <div class="progress-bar bg-danger" role="progressbar" aria-label="Success example" style="width: ${data.stats[1].base_stat}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                          Defense - ${data.stats[2].base_stat}
+                        <div class="progress">
+                            <div class="progress-bar bg-info" role="progressbar" aria-label="Success example" style="width: ${data.stats[2].base_stat}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                          Type - ${data.types[0].type.name}
+                    
+                </div>
+                
+            </div>`;
             }
         });
 }
-
-// container.innerHTML = returnCards();
-
-// after to fetch all the data you tell the loop to stop after getting to 151 of fetching the said data
-
-// miguel run a loop through everysingle pokemon and everytime you run a loop through one create a text node or create the html part of that box setting
-// for example "li: x"
