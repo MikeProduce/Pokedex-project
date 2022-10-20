b13ec7383ede3c8af753d7404d390c9ee7318d7f
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-10-19 19:17:48 -0500

    making modals for extra details for the pokemon

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ for (let i = 1; i <= 151; i++) {
                 const dataForType = data.types[0].type.name;
                 const dataForNameCapitlized = capitlizeFirstLetter(dataForName);
                 document.getElementById("container").innerHTML += `
-                <div class="col-lg-4 col-md-6 col-sm-12 px-5 gy-5">
+                <div class="col-lg-4 col-md-6 col-sm-12 px-5 gy-5 pokemoncard">
                 <img id="image" class="card-img-top" src="${dataForImg}"/>
                 <div class="ui">
                     <h3 id="name">${dataForNameCapitlized}</h3>
@@ -87,11 +87,44 @@ for (let i = 1; i <= 151; i++) {
                                 ></div>
                             </div>
                             <div class="hp">Type - ${dataForType}</div>
+                            <button class="myBtn">Open Modal</button>
+            <div class="myModal modal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <p>Some text in the Modal..</p>
+                </div>
+            </div>
                         </div>
                     </div>
-                </div>`;
+                </div>
+                `;
             }
         });
 }
+
+// Get the modal
+const modal = document.querySelectorAll(".myModal");
+
+// Get the button that opens the modal
+const btn = document.querySelectorAll(".myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.querySelectorAll(".close")[0];
+
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
 
 // miguel add a modal that gives more information
