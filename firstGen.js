150328decf68fc069b22ee52325a8a35ab4f180b
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2022-10-25 10:02:33 -0600

    Fixed all the errors now theres just a delay when the buttons are clicked

--- firstGen.js
+++ firstGen.js
@@ -40,171 +40,111 @@ function showgallary(data) {
         const specDef = data.stats[4].base_stat;
         const specAtt = data.stats[3].base_stat;
 
-        document.getElementById("container").innerHTML += `
-    <div class="col-lg-4 col-md-6 col-sm-12 px-5 gy-5 pokemoncard">
-    <img id="image" class="card-img-top" src="${dataForImg}"/>
+        document.getElementById(
+            "container"
+        ).innerHTML += `<div class="col-lg-4 col-md-6 col-sm-12 px-5 gy-5 pokemoncard">
+    <img class="card-img-top" src="${dataForImg}" />
     <div class="ui">
-        <h3 id="name">${dataForNameCapitlized}</h3>
-            <div class="hp">HP - ${dataForHp} </div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-warning "
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${dataForHp}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100">
-                    </div>
-                </div>
-                <div class="hp">Attack - ${dataForAttack}</div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-danger"
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${dataForAttack}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                    ></div>
-                </div>
-                <div class="hp">Defense - ${dataforDefense}</div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-info"
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${dataforDefense}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                    ></div>
-                </div>
-                <div class="hp">Type - ${dataForType}</div>
-                <button class="modal-open" id="pokemon${dataID}">More Information</button>
-
-    
-                <div id="${dataID}" class="modal">
-
-                <!-- Modal content -->
-                <div class="modal-content">
-                    <span class="close">X</span>
-                <div class="flexmodal">
-                    <img id="image" class="card-img-top" src="${PokeShinyImg}"/>
-                    <div class="left align">
-                    <p>Pokedex number - ${dataID}</p>
-                    <p>Shiny Color</p>
-                    <p>Height - ${pokeHeight}</p>
-                    <p>Weight - ${pokeWeight}</p>
-                    <p>Abilities - ${abilities}</p>
-                </div>
-                </div>
-                <div class="moreStats">
-                <div class="hp">Attack - ${dataForAttack}</div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-danger"
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${dataForAttack}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                    ></div>
-                </div>
-                <div class="hp">Defense - ${dataforDefense}</div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-info"
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${dataforDefense}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                    ></div>
-                </div>
-                <div class="hp">HP - ${dataForHp} </div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-warning "
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${dataForHp}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100">
-                    </div>
-                </div>
-                <div class="hp">Spec Def - ${specDef}</div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-success"
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${specDef}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                    ></div>
-                </div>
-                <div class="hp">Spec Att - ${specAtt}</div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-primary"
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${specAtt}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                    ></div>
-                </div>
-                <div class="hp">Speed - ${speed}</div>
-                <div class="progress">
-                    <div
-                        class="progress-bar bg-dark"
-                        role="progressbar"
-                        aria-label="Success example"
-                        style="width: ${speed}%"
-                        aria-valuenow="25"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                    ></div>
-                </div>
-                </div>
-
-                
-
-                </div>
-                </div>
-
-
-
-                        
+        <h3 class="name">${dataForNameCapitlized}</h3>
+        <div class="hp">HP - ${dataForHp} </div>
+        <div class="progress">
+            <div class="progress-bar bg-warning " role="progressbar" aria-label="Success example"
+                style="width: ${dataForHp}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
             </div>
         </div>
+        <div class="hp">Attack - ${dataForAttack}</div>
+        <div class="progress">
+            <div class="progress-bar bg-danger" role="progressbar" aria-label="Success example"
+                style="width: ${dataForAttack}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <div class="hp">Defense - ${dataforDefense}</div>
+        <div class="progress">
+            <div class="progress-bar bg-info" role="progressbar" aria-label="Success example"
+                style="width: ${dataforDefense}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <div class="hp">Type - ${dataForType}</div>
+        <button class="modal-open" id="pokemon${dataID}">More Information</button>
+
+
+        <div id="${dataID}" class="modal">
+
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close" id="${dataID}">X</span>
+                <div class="flexmodal">
+                    <img class="card-img-top" src="${PokeShinyImg}" />
+                    <div class="left align">
+                        <p>Pokedex number - ${dataID}</p>
+                        <p>Shiny Color</p>
+                        <p>Height - ${pokeHeight}</p>
+                        <p>Weight - ${pokeWeight}</p>
+                        <p>Abilities - ${abilities}</p>
+                    </div>
+                </div>
+                <div class="moreStats">
+                    <div class="hp">Attack - ${dataForAttack}</div>
+                    <div class="progress">
+                        <div class="progress-bar bg-danger" role="progressbar" aria-label="Success example"
+                            style="width: ${dataForAttack}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        </div>
+                    </div>
+                    <div class="hp">Defense - ${dataforDefense}</div>
+                    <div class="progress">
+                        <div class="progress-bar bg-info" role="progressbar" aria-label="Success example"
+                            style="width: ${dataforDefense}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        </div>
+                    </div>
+                    <div class="hp">HP - ${dataForHp} </div>
+                    <div class="progress">
+                        <div class="progress-bar bg-warning " role="progressbar" aria-label="Success example"
+                            style="width: ${dataForHp}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                        </div>
+                    </div>
+                    <div class="hp">Spec Def - ${specDef}</div>
+                    <div class="progress">
+                        <div class="progress-bar bg-success" role="progressbar" aria-label="Success example"
+                            style="width: ${specDef}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <div class="hp">Spec Att - ${specAtt}</div>
+                    <div class="progress">
+                        <div class="progress-bar bg-primary" role="progressbar" aria-label="Success example"
+                            style="width: ${specAtt}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <div class="hp">Speed - ${speed}</div>
+                    <div class="progress">
+                        <div class="progress-bar bg-dark" role="progressbar" aria-label="Success example"
+                            style="width: ${speed}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+
     </div>
-    `;
+</div>
+</div>`;
     }
+}
 
-    function nomoreprint() {
-        for (let a = 1; a <= 2; a++) {
-            const pokemonone = document.getElementById(`pokemon${a}`);
-            // console.log(pokemonone);
-            const closeModal = document.getElementById(`${a}`);
+var theParent = document.querySelector("#container");
+theParent.addEventListener("click", doSomething, false);
 
-            closeModal.addEventListener("click", () => {
-                document.getElementById(`${a}`).style.display = "none";
-            });
-
-            pokemonone.addEventListener("click", () => {
-                document.getElementById(`${a}`).style.display = "block";
-            });
+function doSomething(e) {
+    if (e.target !== e.currentTarget) {
+        var clickedItem = e.target.id;
+        console.log(clickedItem);
+        for (let i = 1; i <= 151; i++) {
+            if (clickedItem == "pokemon" + i) {
+                document.getElementById(i).style.display = "block";
+            }
+            if (clickedItem == i) {
+                document.getElementById(i).style.display = "none";
+            }
+            e.stopPropagation();
         }
     }
-    nomoreprint();
 }
 
 // Miguel you have to fix the size of the images, and then you have to fix all the errors coming out of the eventlinsteners
